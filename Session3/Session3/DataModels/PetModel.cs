57246564f9d7287d5d49c8b7b8fd426cb5215790
jlumawod <joseph.lumawod@softvision.com>
2022-10-21 18:10:43 +0800

    Updated Homework with Facade Model and AAA Pattern

---
+++ Session3/Session3/DataModels/PetModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session3
+{
+
+    public class PetModel
+    {
+        public int id { get; set; }
+        public Category category { get; set; }
+        public string name { get; set; }
+        public List<string> photoUrls { get; set; }
+        public List<Tag> tags { get; set; }
+        public string status { get; set; }
+    }
+
+    public class Category
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Tag
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+}
---
+++ Session3/Session3/Resources/Endpoints.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session3
+{
+    public class Endpoints
+    {
+        public const string baseURL = "https://petstore.swagger.io/v2";
+
+        public static string GetPetById(int id) => $"{baseURL}/pet/{id}";
+
+        public static string PostPet() => $"{baseURL}/pet";
+
+    }
+}
